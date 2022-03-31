0e53fa60ac422eaa3c36ffb5e77e6d63cd3d1251
gilbertosantana24 <gilbertobuja@gmail.com>
2022-03-30 17:26:38 -0700

    userRoutes and userModel
    added user routes and model

---
+++ userRoutes.js
@@ -0,0 +1,53 @@
+const router = require('express').Router();
+const User = require('./userModel');
+
+//GET USERS
+router.get('/users', async (req,res)=> {
+    const user = await User.find();
+    res.send(user)
+})
+
+//GET USERS
+router.get('/:id', async (req,res)=> {
+    const user = await User.findById();
+    res.send(user.username);
+})
+
+
+//CREATE USER
+router.post('/createUser', async (req,res)=> {
+    const movie = await User.create({
+        name: req.body.name,
+        lastname: req.body.lastname,
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    res.send(movie);
+})
+
+//UPDATE A USER
+router.put('/updateUser', async (req,res)=>{
+
+    User.update({
+        name: req.body.name,
+        lastname: req.body.lastname,
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    },
+    {
+        where: {
+            id: req.body.id
+        },
+        returning: true,
+        plain: true
+    }).then(function(result) {
+        res.send(result[1].dataValues);
+    })
+    })
+
+
+
+    module.exports = router;
