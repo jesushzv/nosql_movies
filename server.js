236bf2aa9fb34703d2a448ecde98c6229bf096c3
Jesus Zamora <jhector.zamora@hotmail.com>
2022-04-14 00:04:45 -0700

    Allow cross domain added

--- server.js
+++ server.js
@@ -22,6 +22,16 @@ connectDb();
 
 // Middleware
 app.use(cors());
+
+var allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
+app.use(allowCrossDomain);
+
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
